566e7faac0cb6f070508c70374f31b122cc5db72
mywaythere <1176692585@qq.com>
2023-06-07 12:40:44 +0800

    added interactive features

--- server/api.test.js
+++ server/api.test.js
@@ -1,3 +1,9 @@
+/*const request = require("supertest");
 describe('test for story getting', () => {
-    it('shoud return http code 2000 when retrieving story', done = error)
-})
+    let httpsServer;
+    it('shoud return http code 2000 when retrieving story', done => {
+        request(httpsServer)
+            .get('/stories')
+            .expect(200, done);
+    })
+})*/
